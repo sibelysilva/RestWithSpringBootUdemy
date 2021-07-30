94bf38ce2716b24f251d117dffa8602be4a48248
Sibely <belly.moreira@gmail.com>
2021-07-29 21:56:37 -0300

    Hello SpringBoot and Rest Udemy

---
+++ 00 RestWithSpringBootUdemy Spring Initializer/rest.with.spring.boot.udemy/src/test/java/br/com/erudio/rest/with/spring/boot/udemy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.rest.with.spring.boot.udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
